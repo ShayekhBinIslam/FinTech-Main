f304c4fdd5dd2724179da4c9eb7ef673de935214
ShayekhBinIslam <shikh.bin.islam@gmail.com>
2022-02-18 17:50:10 +0600

    chart fixed

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,191 +1,202 @@
-// import React from 'react';
-// // https://stackblitz.com/edit/drop-down-menu-material-ui
-// // import DropDownMenu from 'material-ui/DropDownMenu';
-// // import MenuItem from 'material-ui/MenuItem';
-// // https://mui.com/components/menus/
-// // https://blog.griddynamics.com/using-d3-js-with-react-js-an-8-step-comprehensive-manual/
+import React from 'react';
+// https://stackblitz.com/edit/drop-down-menu-material-ui
+// import DropDownMenu from 'material-ui/DropDownMenu';
+// import MenuItem from 'material-ui/MenuItem';
+// https://mui.com/components/menus/
+// https://blog.griddynamics.com/using-d3-js-with-react-js-an-8-step-comprehensive-manual/
 
-// import { styled, alpha } from '@mui/material/styles';
-// import Button from '@mui/material/Button';
-// import Menu from '@mui/material/Menu';
-// import MenuItem from '@mui/material/MenuItem';
-// import EditIcon from '@mui/icons-material/Edit';
-// import Divider from '@mui/material/Divider';
-// import ArchiveIcon from '@mui/icons-material/Archive';
-// import FileCopyIcon from '@mui/icons-material/FileCopy';
-// import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-// import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { styled, alpha } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import EditIcon from '@mui/icons-material/Edit';
+import Divider from '@mui/material/Divider';
+import ArchiveIcon from '@mui/icons-material/Archive';
+import FileCopyIcon from '@mui/icons-material/FileCopy';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
-// // https://codesandbox.io/s/github/reactchartjs/react-chartjs-2/tree/master/sandboxes/line/default?from-embed=&file=/App.tsx:236-348
-// import {
-//   Chart as ChartJS,
-//   CategoryScale,
-//   LinearScale,
-//   PointElement,
-//   LineElement,
-//   Title,
-//   Tooltip,
-//   Legend,
-// } from 'chart.js';
-// import { Line } from 'react-chartjs-2';
-// import faker from 'faker';
-
-
-// import ButtonGroup from '@mui/material/ButtonGroup';
-// // import rd3 from 'react-d3-library';
-// // import * as d3 from "d3";
+// https://codesandbox.io/s/github/reactchartjs/react-chartjs-2/tree/master/sandboxes/line/default?from-embed=&file=/App.tsx:236-348
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import faker from 'faker';
 
 
-// import "./chart.css";
-// // import rd3 from 'react-d3-library';
-// // import * as d3 from 'd3';
-
-// ChartJS.register(
-//   CategoryScale,
-//   LinearScale,
-//   PointElement,
-//   LineElement,
-//   Title,
-//   Tooltip,
-//   Legend
-// );
-
-// export const options = {
-//   responsive: true,
-//   plugins: {
-//     legend: {
-//       // position: 'top' as const,
-//       position: 'top',
-//     },
-//     title: {
-//       display: true,
-//       text: 'Chart.js Line Chart',
-//     },
-//   },
-// };
-
-// const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-// export const data = {
-//   labels,
-//   datasets: [
-//     {
-//       label: 'Dataset 1',
-//       data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-//       borderColor: 'rgb(255, 99, 132)',
-//       backgroundColor: 'rgba(255, 99, 132, 0.5)',
-//     },
-//     {
-//       label: 'Dataset 2',
-//       data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-//       borderColor: 'rgb(53, 162, 235)',
-//       backgroundColor: 'rgba(53, 162, 235, 0.5)',
-//     },
-//   ],
-// };
+import ButtonGroup from '@mui/material/ButtonGroup';
+// import rd3 from 'react-d3-library';
+// import * as d3 from "d3";
 
 
+import "./chart.css";
+// import rd3 from 'react-d3-library';
+// import * as d3 from 'd3';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      // position: 'top' as const,
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
 
 
-// const StyledMenu = styled((props) => (
-//   <Menu
-//     elevation={0}
-//     anchorOrigin={{
-//       vertical: 'bottom',
-//       horizontal: 'right',
-//     }}
-//     transformOrigin={{
-//       vertical: 'top',
-//       horizontal: 'right',
-//     }}
-//     {...props}
-//   />
-// ))(({ theme }) => ({
-//   '& .MuiPaper-root': {
-//     borderRadius: 6,
-//     marginTop: theme.spacing(1),
-//     minWidth: 180,
-//     color:
-//       theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
-//     boxShadow:
-//       'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-//     '& .MuiMenu-list': {
-//       padding: '4px 0',
-//     },
-//     '& .MuiMenuItem-root': {
-//       '& .MuiSvgIcon-root': {
-//         fontSize: 18,
-//         color: theme.palette.text.secondary,
-//         marginRight: theme.spacing(1.5),
-//       },
-//       '&:active': {
-//         backgroundColor: alpha(
-//           theme.palette.primary.main,
-//           theme.palette.action.selectedOpacity,
-//         ),
-//       },
-//     },
-//   },
-// }));
 
 
-// export default function Chart() {
+const StyledMenu = styled((props) => (
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'right',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'right',
+    }}
+    {...props}
+  />
+))(({ theme }) => ({
+  '& .MuiPaper-root': {
+    borderRadius: 6,
+    marginTop: theme.spacing(1),
+    minWidth: 180,
+    color:
+      theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
+    boxShadow:
+      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+    '& .MuiMenu-list': {
+      padding: '4px 0',
+    },
+    '& .MuiMenuItem-root': {
+      '& .MuiSvgIcon-root': {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      '&:active': {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity,
+        ),
+      },
+    },
+  },
+}));
+
+
+export default function Chart() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  // The plot
+  // var node = document.createElement('div');
   
-//   return (
-//     <div className='chart'>
-//       <h3 className="chartTitle">Income Graph</h3>
-//       <div
+  return (
+    <div className='chart'>
+      <h3 className="chartTitle">Income Graph</h3>
+      <div
 
-//       >
-//         {/* Monthly or yearly */}
-//         <ButtonGroup variant="contained" aria-label="outlined primary button group"
+      >
+        {/* Monthly or yearly */}
+        <ButtonGroup variant="contained" aria-label="outlined primary button group"
 
-//         >
-//           <Button>Monthly</Button>
-//           <Button>Yearly</Button>
-//         </ButtonGroup>
-//       </div>
+        >
+          <Button>Monthly</Button>
+          <Button>Yearly</Button>
+        </ButtonGroup>
+      </div>
 
-//       {/* Selected span: monthly: 6 month, yearly: 4-5 years */}
-//       <div>
-//         <Button
-//           id="demo-customized-button"
-//           aria-controls={open ? 'demo-customized-menu' : undefined}
-//           aria-haspopup="true"
-//           aria-expanded={open ? 'true' : undefined}
-//           variant="contained"
-//           disableElevation
-//           onClick={handleClick}
-//           endIcon={<KeyboardArrowDownIcon />}
-//         >
-//           Options
-//         </Button>
-//         <StyledMenu
-//           id="demo-customized-menu"
-//           MenuListProps={{
-//             'aria-labelledby': 'demo-customized-button',
-//           }}
-//           anchorEl={anchorEl}
-//           open={open}
-//           onClose={handleClose}
-//         >
-//           <MenuItem
-//           onClick={handleClose}
-//           disableRipple>
-//             <EditIcon />
-//             6 month
-//           </MenuItem>
-//           <MenuItem
-//           onClick={handleClose}
-//           disableRipple>
-//             <FileCopyIcon />
-//             10 month
-//           </MenuItem>
-//         </StyledMenu>
-//       </div>
-//       {/* Line plot */}
-//       <Line options={options} data={data} />
+      {/* Selected span: monthly: 6 month, yearly: 4-5 years */}
+      <div>
+        <Button
+          id="demo-customized-button"
+          aria-controls={open ? 'demo-customized-menu' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          variant="contained"
+          disableElevation
+          onClick={handleClick}
+          endIcon={<KeyboardArrowDownIcon />}
+        >
+          Options
+        </Button>
+        <StyledMenu
+          id="demo-customized-menu"
+          MenuListProps={{
+            'aria-labelledby': 'demo-customized-button',
+          }}
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+        >
+          <MenuItem
+          onClick={handleClose}
+          disableRipple>
+            <EditIcon />
+            6 month
+          </MenuItem>
+          <MenuItem
+          onClick={handleClose}
+          disableRipple>
+            <FileCopyIcon />
+            10 month
+          </MenuItem>
+        </StyledMenu>
+      </div>
+      {/* Line plot */}
+      <Line options={options} data={data} />
 
-//     </div>
-//   );
-// }
+    </div>
+  );
+}
