301e6bbcbb82200d20377e31f847969f6b3beea9
itowhid <45915910+itowhid@users.noreply.github.com>
2022-02-20 01:10:42 +0600

    only client

--- src/models/User.js
+++ src/models/User.js
@@ -8,17 +8,17 @@ module.exports = class User{
         this.name = name;
         this.email = email;
     }
-    static getIncome(clientID){
-        return db.execute("SELECT * FROM INCOME WHERE clientID='" + clientID +"' ");
-    }
-    static addNewIncome(income){
-        //THIS CAN BE EDITED ACCORDING TO PASSING INFO
-        //this.incomes.
-        //db.execute("INSERT INTO INCOME(TITLE,AMMOUNT,DESCRIPTION,CATAGORY) VALUES ('"+ title+ "','"+ ammount+ "','"+ description+ "','"+ catagory+ "')");
-    }
-    static updateIncome(title,ammount){
-        return income.editIncome(title,ammount,this.id);
-    }
+    // static getIncome(clientID){
+    //     return db.execute("SELECT * FROM INCOME WHERE clientID='" + clientID +"' ");
+    // }
+    // static addNewIncome(income){
+    //     //THIS CAN BE EDITED ACCORDING TO PASSING INFO
+    //     //this.incomes.
+    //     //db.execute("INSERT INTO INCOME(TITLE,AMMOUNT,DESCRIPTION,CATAGORY) VALUES ('"+ title+ "','"+ ammount+ "','"+ description+ "','"+ catagory+ "')");
+    // }
+    // static updateIncome(title,ammount){
+    //     return income.editIncome(title,ammount,this.id);
+    // }
 
     static loginAuth(username, password, email){
         // pool.query('select * from client where clientid=?', [],(err, result, fields) => {
@@ -28,6 +28,6 @@ module.exports = class User{
         //     return console.log(result);
         // })
 
-        console.log(db.execute("SELECT * FROM CLIENT"));
+        // console.log(db.execute("SELECT * FROM CLIENT"));
     }
 }
